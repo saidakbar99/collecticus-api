bce58203b81dc50e92a351c3448aca21e6df0960
Saidakbar Zafarov <saidakbar.dev@gmail.com>
2024-01-26 03:43:16 +0500

    changed cors

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({
+    credentials: true,
+    origin: 'https://collecticus.onrender.com',
+}))
 app.use('/api', router)
 
 const start = async () => {
