ab3e48fb046caf071ec1504582750ab8095a7a40
Saidakbar Zafarov <saidakbar.dev@gmail.com>
2024-01-26 03:24:56 +0500

    changed cors

--- index.js
+++ index.js
@@ -13,10 +13,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({
-    credentials: true,
-    origin: process.env.CLIENT_URL,
-}))
+app.use(cors())
 app.use('/api', router)
 
 const start = async () => {
